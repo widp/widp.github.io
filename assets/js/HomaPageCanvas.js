6d0b16ab40a784150f82f353d6075515aec77e0b
tejaswidp <tejaswidprakash@gmail.com>
2017-12-18 18:34:40 +0530

    added static, the webpage is quite unusable at this point.

--- assets/js/HomaPageCanvas.js
+++ assets/js/HomaPageCanvas.js
@@ -1,7 +1,34 @@
 
 function init(){
 
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    // create Oscillator node
+    var oscillator = audioCtx.createOscillator();
+
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 440; // value in hertz
+    oscillator.connect(audioCtx.destination);
+    whitenoisegen(audioCtx);
+
+
     window.requestAnimationFrame(draw);
+    
+}
+function whitenoisegen(audioContext) {
+//    var bufferSize = 4096;
+    var bufferSize  = 256;
+    var whiteNoise = audioContext.createScriptProcessor(bufferSize, 1, 1);
+    whiteNoise.onaudioprocess = function(e) {
+	var output = e.outputBuffer.getChannelData(0);
+	for (var i = 0; i < bufferSize; i++) {
+            output[i] = Math.random() * 2 - 1;
+	}
+    }
+    var gainNode  = audioContext.createGain();
+    gainNode.gain.value = 0.008;
+    whiteNoise.connect(gainNode);
+    gainNode.connect(audioContext.destination);
 }
 
 function draw() {
@@ -20,17 +47,12 @@ function draw() {
 	ctx.font = '12px serif';
 	var text = document.getElementById('maintext').textContent;
 	ctx.textBaseline  = "top";
-//	ctx.textAlign = "center";
-//	ctx.fillText(text , 100,100);
 
-
-//	for(i = 0; i < Math.random()*10 ; i++) {
 
 	var r = 0 , b = 0, g = 0, a = 0.5;
-	    ctx.fillStyle = 'rgb('+ r +','+ g + ',' + b + ')';
-	    ctx.fillRect(Math.random(),Math.random()*10000,Math.random()*10000,Math.random()*100);
+	ctx.fillStyle = 'rgb('+ r +','+ g + ',' + b + ')';
+	ctx.fillRect(Math.random(),Math.random()*10000,Math.random()*10000,Math.random()*100);
 
-//	}
 
 	window.requestAnimationFrame(draw);
     }
